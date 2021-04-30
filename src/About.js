66a0673e5aa9ef767eb3058c5e6bfd7e4a4fa1cd
Minee <mingramer@gmail.com>
2021-04-30 23:10:54 +0900

    리덕스복스

--- src/About.js
+++ src/About.js
@@ -10,7 +10,7 @@ const About = ({ location }) => {
   return (
     <div>
       <h1>소개</h1>
-      <p>이 프로젝트는 리액트 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      <p>이 프로젝트는 리액트 기초를 실습해보는 예제 프로젝트입니다.</p>
       {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
     </div>
   );
